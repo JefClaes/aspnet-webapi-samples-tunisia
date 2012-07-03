ebacc9a895ea4a7632641443a86881e772c5afb5
Jef Claes <jef.claes@gmail.com>
2012-07-03 21:50:38 +0200

    Created projects

---
+++ src/Samples.1.Server/Samples.1.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Samples._1.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
