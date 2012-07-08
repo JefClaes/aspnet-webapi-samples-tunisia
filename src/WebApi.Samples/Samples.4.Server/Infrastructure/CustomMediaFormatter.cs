36224a37a9fe3b6685018438e4e7e1a7f7c5ec5f
Jef Claes <jef.claes@gmail.com>
2012-07-08 22:51:37 +0200

    Added content negotiation project
    Added content negotiation example w custom media formatter

---
+++ src/WebApi.Samples/Samples.4.Server/Infrastructure/CustomMediaFormatter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http.Formatting;
+using Samples.Common;
+using System.IO;
+
+namespace Samples._4.Server.Infrastructure
+{
+    public class CustomMediaFormatter : BufferedMediaTypeFormatter
+    {
+        public CustomMediaFormatter()
+        {
+            SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/example"));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            return false;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            if (type == typeof(Resume))
+                return true;
+
+            return false;
+        }
+
+        public override void WriteToStream(Type type, object value, System.IO.Stream stream, System.Net.Http.Headers.HttpContentHeaders contentHeaders)
+        {
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.Write("CustomFormatted resume");
+            }
+        }
+    }
+}
---
+++ src/WebApi.Samples/Samples.4.Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
+using Samples._4.Server.Infrastructure;
+
+namespace Samples._4.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+            config.Formatters.Add(new CustomMediaFormatter());
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            using (var server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
