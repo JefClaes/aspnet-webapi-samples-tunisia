6b307c8ee17907e5d03de41bc3a3c563d03db46a
Jef Claes <jef.claes@gmail.com>
2012-07-11 07:40:47 +0200

    Added HttpMessageHandler project

---
+++ src/WebApi.Samples/Samples.5.Server/Infrastructure/MethodOverrideHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Samples._5.Server.Infrastructure
+{
+    public class MethodOverrideHandler : DelegatingHandler
+    {
+        readonly string[] _methods = { "DELETE", "HEAD", "PUT" };
+        const string _header = "X-HTTP-Method-Override";
+
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // Check for HTTP POST with the X-HTTP-Method-Override header.
+            if (request.Method == HttpMethod.Post && request.Headers.Contains(_header))
+            {
+                // Check if the header value is in our methods list.
+                var method = request.Headers.GetValues(_header).FirstOrDefault();
+                if (_methods.Contains(method, StringComparer.InvariantCultureIgnoreCase))
+                {
+                    // Change the request method.
+                    request.Method = new HttpMethod(method);
+                }
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ src/WebApi.Samples/Samples.5.Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
+using Samples._5.Server.Infrastructure;
+
+namespace Samples._5.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+            config.MessageHandlers.Add(new MethodOverrideHandler());
+
+            using (var server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
