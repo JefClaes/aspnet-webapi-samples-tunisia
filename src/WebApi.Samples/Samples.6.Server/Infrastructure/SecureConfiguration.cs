4be4457829c9fa920d8ca821e4443ae7b286e24e
Jef Claes <jef.claes@gmail.com>
2012-07-18 07:27:55 +0200

    Added security project.

---
+++ src/WebApi.Samples/Samples.6.Server/Infrastructure/SecureConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http.SelfHost;
+using System.Web.Http.SelfHost.Channels;
+using System.ServiceModel.Channels;
+
+namespace Samples._6.Server.Infrastructure
+{
+    public class SecureConfiguration : HttpSelfHostConfiguration
+    {
+        public SecureConfiguration(string baseAdress) : base(baseAdress) { }
+
+        protected override BindingParameterCollection OnConfigureBinding(HttpBinding httpBinding)
+        {
+            httpBinding.Security.Mode = System.Web.Http.SelfHost.Channels.HttpBindingSecurityMode.TransportCredentialOnly;
+            httpBinding.Security.Transport.ClientCredentialType = System.ServiceModel.HttpClientCredentialType.Windows;
+            
+            var bc = base.OnConfigureBinding(httpBinding);
+
+            return bc;
+        }
+    }
+}
