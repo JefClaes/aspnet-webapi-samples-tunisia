014162884ae76d58c1035612b5d920d27f443db9
Jef Claes <jef.claes@gmail.com>
2012-08-07 07:50:41 +0200

    Added tests

---
+++ src/WebApi.EuricomCruise.Samples/WebApi.EuricomCruise.Samples.Tests/BankControllerIntegrationTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using WebApi.EuricomCruise.Samples.Server.Infrastructure;
+using System.Net.Http;
+using WebApi.EuricomCruise.Samples.Server;
+using System.Net;
+
+namespace WebApi.EuricomCruise.Samples.Tests
+{
+    [TestClass()]
+    public class BankControllerIntegrationTest
+    {
+        private TestContext testContextInstance;
+
+        [TestMethod()]
+        public void Put_throws_not_implemented_exception()
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:8080");
+
+            config.IncludeErrorDetailPolicy = IncludeErrorDetailPolicy.Never;
+            config.Filters.Add(new NotImplementedErrorFilter());
+            config.Routes.MapHttpRoute(
+                "DefaultApi", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+            config.Formatters.Add(new EuricomFormatter());
+            config.MessageHandlers.Add(new MethodOverrideHandler());
+            config.DependencyResolver = new DependencyResolver();
+
+            var server = new HttpServer(config);
+            var client = new HttpClient(server);
+
+            var result = client.PutAsXmlAsync<Bank>("http://localhost:8080/api/bank", new Bank()).Result;
+
+            Assert.AreEqual(HttpStatusCode.NotImplemented, result.StatusCode);
+        }
+    }
+}
---
+++ src/WebApi.EuricomCruise.Samples/WebApi.EuricomCruise.Samples.Tests/BankControllerUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using WebApi.EuricomCruise.Samples.Server.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WebApi.EuricomCruise.Samples.Server;
+
+namespace WebApi.EuricomCruise.Samples.Tests
+{    
+    [TestClass()]
+    public class BankControllerUnitTest
+    {
+        private TestContext testContextInstance;       
+        
+        [TestMethod()]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void Put_throws_not_implemented_exception()
+        {
+            var bankController = new BankController(new BankStore());
+
+            bankController.PutBank("1", new Bank() { BIC = "1", Name = "KBC" });            
+        }
+    }
+}
